10b8b7cf81d5d0076b9bb56efabe9c64ace9d50b
asmodat <asmodat@hotmail.com>
2018-08-25 15:03:07 +0200

    2018-08-25

--- EC2/EC2Helper.cs
+++ EC2/EC2Helper.cs
@@ -14,12 +14,40 @@ namespace AWSWrapper.EC2
     public partial class EC2Helper
     {
         private readonly int _maxDegreeOfParalelism;
-        internal readonly AmazonEC2Client _EC2Client;
+        internal readonly AmazonEC2Client _client;
+
+        public enum AMIStatus
+        {
+            available = 1,
+            pending = 1 << 1,
+            failed = 1 << 2
+        }
+
+        public enum InstanceStateCode
+        {
+            pending = 0,
+            running = 16,
+            terminating = 32,
+            terminated = 48,
+            stopping = 64,
+            stopped = 80
+        }
+
+        public enum InstanceModel
+        {
+            T2Nano = 1,
+            T2Micro,
+            T2Small,
+            T2Medium,
+            T2Large,
+            T2XLarge,
+            T22XLarge
+        }
 
         public EC2Helper(int maxDegreeOfParalelism = 8)
         {
             _maxDegreeOfParalelism = maxDegreeOfParalelism;
-            _EC2Client = new AmazonEC2Client();
+            _client = new AmazonEC2Client();
         }
 
         public async Task<Reservation[]> DescribeInstancesAsync(List<string> instanceIds = null, Dictionary<string, List<string>> filters = null, CancellationToken cancellationToken = default(CancellationToken))
@@ -29,7 +57,7 @@ namespace AWSWrapper.EC2
             string nextToken = null;
             DescribeInstancesResponse response;
             var results = new List<Reservation>();
-            while ((response = await _EC2Client.DescribeInstancesAsync(new DescribeInstancesRequest()
+            while ((response = await _client.DescribeInstancesAsync(new DescribeInstancesRequest()
             {
                 MaxResults = 1000,
                 NextToken = nextToken,
@@ -52,21 +80,81 @@ namespace AWSWrapper.EC2
             return results.ToArray();
         }
 
+        public Task<RunInstancesResponse> CreateInstanceAsync(
+            string imageId,
+            InstanceType instanceType,
+            string keyName,
+            string securityGroupId,
+            string subnetId,
+            string roleName,
+            ShutdownBehavior shutdownBehavior,
+            bool associatePublicIpAddress,
+            Dictionary<string, string> tags,
+            CancellationToken cancellationToken = default(CancellationToken))
+            => _client.RunInstancesAsync(new RunInstancesRequest()
+            {
+                ImageId = imageId,
+                InstanceType = instanceType,
+                MinCount = 1,
+                MaxCount = 1,
+                KeyName = keyName,
+                InstanceInitiatedShutdownBehavior = shutdownBehavior,
+                DisableApiTermination = false,
+                IamInstanceProfile = roleName.IsNullOrEmpty() ? null : new IamInstanceProfileSpecification() {
+                    Name = roleName
+                },
+                NetworkInterfaces = new List<InstanceNetworkInterfaceSpecification>()
+                {
+                    new InstanceNetworkInterfaceSpecification()
+                    {
+                        DeviceIndex = 0,
+                        SubnetId = subnetId,
+                        Groups = new List<string>() { securityGroupId },
+                        AssociatePublicIpAddress = associatePublicIpAddress,
+                        Description = "Primary network interface",
+                        DeleteOnTermination = true,
+                    }
+                },
+                TagSpecifications = new List<TagSpecification>()
+                {
+                    new TagSpecification(){
+                        ResourceType =  ResourceType.Instance,
+                        Tags = tags.Select(x => new Tag(){ Key = x.Key, Value = x.Value }).ToList()
+                    }
+                },
+            }, cancellationToken).EnsureSuccessAsync();
+
+
+        public async Task<InstanceStatus> DescribeInstanceStatusAsync(string instanceId, CancellationToken cancellationToken = default(CancellationToken))
+            => (await _client.DescribeInstanceStatusAsync(new DescribeInstanceStatusRequest()
+            {
+                InstanceIds = new List<string>() { instanceId },
+                IncludeAllInstances = true
+            }, cancellationToken).EnsureSuccessAsync()).InstanceStatuses.SingleOrDefault();
+
+        public async Task<Image> DescribeImageAsync(string name, AMIStatus state = AMIStatus.available, CancellationToken cancellationToken = default(CancellationToken))
+            => (await _client.DescribeImagesAsync(new DescribeImagesRequest()
+            {
+                Filters = new List<Filter>() {
+                    new Filter() { Name = "name", Values = new List<string>() { name } },
+                    new Filter() { Name = "state", Values = new List<string>() { state.ToString() } } }
+            }, cancellationToken).EnsureSuccessAsync()).Images.SingleOrDefault();
+
         public Task<StartInstancesResponse> StartInstancesAsync(List<string> instanceIds, string additionalInfo = null, CancellationToken cancellationToken = default(CancellationToken))
-            => _EC2Client.StartInstancesAsync(new StartInstancesRequest() {
+            => _client.StartInstancesAsync(new StartInstancesRequest() {
                 InstanceIds = instanceIds,
                 AdditionalInfo = additionalInfo
             }, cancellationToken).EnsureSuccessAsync();
 
         public Task<StopInstancesResponse> StopInstancesAsync(List<string> instanceIds, bool force = false, CancellationToken cancellationToken = default(CancellationToken))
-            => _EC2Client.StopInstancesAsync(new StopInstancesRequest()
+            => _client.StopInstancesAsync(new StopInstancesRequest()
             {
                 InstanceIds = instanceIds,
                 Force = force,
             }, cancellationToken).EnsureSuccessAsync();
 
         public Task<TerminateInstancesResponse> TerminateInstancesAsync(List<string> instanceIds, CancellationToken cancellationToken = default(CancellationToken))
-            => _EC2Client.TerminateInstancesAsync(new TerminateInstancesRequest()
+            => _client.TerminateInstancesAsync(new TerminateInstancesRequest()
             {
                 InstanceIds = instanceIds
             }, cancellationToken).EnsureSuccessAsync();
--- EC2/EC2HelperEx.cs
+++ EC2/EC2HelperEx.cs
@@ -4,11 +4,49 @@ using Amazon.EC2.Model;
 using AsmodatStandard.Extensions.Collections;
 using System.Linq;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System;
+using static AWSWrapper.EC2.EC2Helper;
+using AsmodatStandard.Extensions;
+using Amazon.EC2;
 
 namespace AWSWrapper.EC2
 {
     public static class EC2HelperEx
     {
+        public static InstanceType ToInstanceType(this InstanceModel model)
+        {
+            switch(model)
+            {
+                case InstanceModel.T2Nano: return InstanceType.T2Nano;
+                case InstanceModel.T2Micro: return InstanceType.T2Micro;
+                case InstanceModel.T2Small: return InstanceType.T2Small;
+                case InstanceModel.T2Medium: return InstanceType.T2Medium;
+                case InstanceModel.T2Large: return InstanceType.T2Large;
+                case InstanceModel.T2XLarge: return InstanceType.T2Xlarge;
+                case InstanceModel.T22XLarge: return InstanceType.T22xlarge;
+                default: throw new Exception($"Unrecognized instance model: {model.ToString()}");
+            }
+        }
+
+        public static async Task AwaitInstanceStateCode(this EC2Helper ec2, string instanceId, InstanceStateCode instanceStateCode, int timeout_ms, int intensity = 1500, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var sw = Stopwatch.StartNew();
+            InstanceStatus status = null;
+            do
+            {
+                if (status != null)
+                    await Task.Delay(intensity);
+
+                status = await ec2.DescribeInstanceStatusAsync(instanceId, cancellationToken);
+                if (status.InstanceState.Code == (int)instanceStateCode)
+                    return;
+            }
+            while (sw.ElapsedMilliseconds < timeout_ms);
+
+            throw new TimeoutException($"Instance {instanceId} could not reach state code {instanceStateCode.ToString()}, last state: {status?.InstanceState?.Code.ToEnumStringOrDefault<InstanceStateCode>($"<convertion failure of value {status?.InstanceState?.Code}>")}");
+        }
+
         public static string GetTagValueOrDefault(this Instance instance, string key)
             => instance.Tags?.FirstOrDefault(x => x.Key == key)?.Value;
 
@@ -22,6 +60,12 @@ namespace AWSWrapper.EC2
         {
             var batch = await ec2.DescribeInstancesAsync(instanceIds: null, filters: null, cancellationToken: cancellationToken);
             return batch.SelectMany(x => x.Instances).Where(x => (x?.Tags?.Any(t => t?.Key == tagKey) ?? false) == true).ToArray();
+        }
+
+        public static async Task<Instance[]> ListInstancesByName(this EC2Helper ec2, string name, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var batch = await ec2.DescribeInstancesAsync(instanceIds: null, filters: null, cancellationToken: cancellationToken);
+            return batch.SelectMany(x => x.Instances).Where(x => (x?.Tags?.Any(t => t?.Key?.ToLower() == "name" && t.Value == name) ?? false) == true).ToArray();
         }
 
         public static async Task<InstanceStateChange> StopInstance(this EC2Helper ec2,string instanceId, bool force = false, CancellationToken cancellationToken = default(CancellationToken))
